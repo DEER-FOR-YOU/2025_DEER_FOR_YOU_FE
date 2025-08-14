8d4af0952b88a27fcdf3d3f3a4824c5c7336c348
chahyunwoo00 <ckgusdn8@naver.com>
2025-08-14 21:48:50 +0900

    [#3] Feat: 아티스트 라우트 추가

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,10 +1,17 @@
 import React, { lazy, Suspense } from 'react';
 import { createBrowserRouter } from 'react-router-dom';
 
+const Artist = lazy(() => import('./pages/artist'));
+
 const withSuspense = (element: React.ReactNode) => (
   <Suspense fallback={<></>}>{element}</Suspense>
 );
 
-const router = createBrowserRouter([]);
+const router = createBrowserRouter([
+  {
+    path: '/artist',
+    element: withSuspense(<Artist />),
+  },
+]);
 
 export default router;
