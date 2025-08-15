bcced0906c3ec720f1667e4b3c19bf9574127893
chahyunwoo00 <ckgusdn8@naver.com>
2025-08-15 17:48:45 +0900

    [#3] Feat: Navbar 구현

--- src/layouts/DefaultLayout.tsx
+++ src/layouts/DefaultLayout.tsx
@@ -1,9 +1,13 @@
-import { Outlet } from 'react-router-dom';
+import { Outlet, useLocation } from 'react-router-dom';
+import Navbar from '../components/navbar';
 
 const DefaultLayout = () => {
+  const location = useLocation();
+  const showNavbar = ['/artist'].includes(location.pathname);
   return (
     <>
       <Outlet />
+      {showNavbar && <Navbar />}
     </>
   );
 };
