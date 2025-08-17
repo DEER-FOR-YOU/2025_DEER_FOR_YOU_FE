7f69336cbc922fc82c186b298984198f2cf2f0ad
chahyunwoo00 <ckgusdn8@naver.com>
2025-08-17 22:23:19 +0900

    file: 폰트 파일 경로 변경

--- src/styles/fonts.ts
+++ src/styles/fonts.ts
@@ -6,13 +6,13 @@ export const fonts = css`
     font-style: normal;
     line-height: 150%;
     letter-spacing: -5%;
-    src: url('../fonts/NeoDunggeunmoPro-Regular.woff2') format('woff2');
+    src: url('/fonts/NeoDunggeunmoPro-Regular.woff2') format('woff2');
   }
   @font-face {
     font-family: 'Pretendard';
     font-style: normal;
     line-height: 150%;
     letter-spacing: -2.5%;
-    src: url('../fonts/PretendardVariable.woff2') format('woff2');
+    src: url('/fonts/PretendardVariable.woff2') format('woff2');
   }
 `;
