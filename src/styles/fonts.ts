3af3425052850e29f066b2d425d5017a65b8a668
chahyunwoo00 <ckgusdn8@naver.com>
2025-08-17 22:45:42 +0900

    chore: 폰트 src 경로 수정

--- src/styles/fonts.ts
+++ src/styles/fonts.ts
@@ -4,15 +4,21 @@ export const fonts = css`
   @font-face {
     font-family: 'NeoDunggeunmoPro';
     font-style: normal;
+    font-weight: 400;
     line-height: 150%;
     letter-spacing: -5%;
-    src: url('/fonts/NeoDunggeunmoPro-Regular.woff2') format('woff2');
+    src: url(${process.env.PUBLIC_URL}/fonts/NeoDunggeunmoPro-Regular.woff2)
+      format('woff2');
+    font-display: swap;
   }
   @font-face {
     font-family: 'Pretendard';
     font-style: normal;
+    font-weight: 400;
     line-height: 150%;
     letter-spacing: -2.5%;
-    src: url('/fonts/PretendardVariable.woff2') format('woff2');
+    src: url(${process.env.PUBLIC_URL}/fonts/PretendardVariable.woff2)
+      format('woff2');
+    font-display: swap;
   }
 `;
