881e2c2ed1549d959cac47ab61e89c73ecd3b440
chahyunwoo00 <ckgusdn8@naver.com>
2025-08-18 23:06:21 +0900

    feat: 모든페이지 화면 최상단으로 스크롤

--- src/layouts/DefaultLayout.tsx
+++ src/layouts/DefaultLayout.tsx
@@ -1,15 +1,23 @@
 import { Outlet, useLocation } from 'react-router-dom';
+import { useEffect } from 'react';
 import Navbar from '../components/navbar';
 import { ROUTES } from '../constants/routes';
 
 const DefaultLayout = () => {
   const location = useLocation();
+
+  // 라우트 변경 시 스크롤을 최상단으로 이동
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [location.pathname]);
+
   const showNavbar = [
     ROUTES.ARTISTS,
     ROUTES.GOODS,
     ROUTES.HOME,
     ROUTES.MAP,
   ].includes(location.pathname);
+
   return (
     <>
       <Outlet />
