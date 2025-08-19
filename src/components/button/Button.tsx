bf44315a2c6eebd1161256a267d8858a81ea11be
chahyunwoo00 <ckgusdn8@naver.com>
2025-08-19 14:12:10 +0900

    feat: 버튼 공통컴포넌트 제작

---
+++ src/components/button/Button.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import * as S from './button.style';
+import type { ButtonHTMLAttributes, PropsWithChildren } from 'react';
+import type { css as cssType } from '@emotion/react';
+
+interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
+  isActive?: boolean;
+  css?: ReturnType<typeof cssType>;
+}
+
+export default function Button({
+  children,
+  isActive = false,
+  type = 'button',
+  css,
+  ...props
+}: PropsWithChildren<ButtonProps>) {
+  return (
+    <S.ButtonWrapper isActive={isActive} type={type} {...props}>
+      <S.ButtonWrapper2 isActive={isActive}>
+        <S.ButtonWrapper3 isActive={isActive}>
+          <S.ButtonWrapper4 isActive={isActive}>
+            <S.ButtonContainer isActive={isActive} css={css}>
+              {children}
+            </S.ButtonContainer>
+          </S.ButtonWrapper4>
+        </S.ButtonWrapper3>
+      </S.ButtonWrapper2>
+    </S.ButtonWrapper>
+  );
+}
---
+++ src/components/button/button.style.ts
@@ -0,0 +1,113 @@
+import styled from '@emotion/styled';
+import { css } from '@emotion/react';
+import type { css as cssType } from '@emotion/react';
+
+export const ButtonWrapper = styled.button<{ isActive: boolean }>`
+  border-bottom: 2px solid #252528;
+  border-right: 2px solid #252528;
+  transition: all 0.1s ease;
+
+  &:active {
+    border-bottom: none;
+    border-right: none;
+    border-top: 2px solid #252528;
+    border-left: 2px solid #252528;
+  }
+
+  ${({ isActive }) =>
+    isActive &&
+    css`
+      border-bottom: none;
+      border-right: none;
+      border-top: 2px solid #252528;
+      border-left: 2px solid #252528;
+    `}
+`;
+
+export const ButtonWrapper2 = styled.div<{ isActive: boolean }>`
+  border-top: 2px solid rgb(212, 217, 222);
+  border-left: 2px solid rgb(212, 217, 222);
+  transition: all 0.1s ease;
+
+  &:active {
+    border-top: none;
+    border-left: none;
+    border-bottom: 2px solid rgb(212, 217, 222);
+    border-right: 2px solid rgb(212, 217, 222);
+  }
+
+  ${({ isActive }) =>
+    isActive &&
+    css`
+      border-top: none;
+      border-left: none;
+      border-bottom: 2px solid rgb(212, 217, 222);
+      border-right: 2px solid rgb(212, 217, 222);
+    `}
+`;
+
+export const ButtonWrapper3 = styled.div<{ isActive: boolean }>`
+  border-bottom: 2px solid rgb(92, 96, 100);
+  border-right: 2px solid rgb(92, 96, 100);
+  transition: all 0.1s ease;
+
+  &:active {
+    border-bottom: none;
+    border-right: none;
+    border-top: 2px solid rgb(92, 96, 100);
+    border-left: 2px solid rgb(92, 96, 100);
+  }
+
+  ${({ isActive }) =>
+    isActive &&
+    css`
+      border-bottom: none;
+      border-right: none;
+      border-top: 2px solid rgb(92, 96, 100);
+      border-left: 2px solid rgb(92, 96, 100);
+    `}
+`;
+
+export const ButtonWrapper4 = styled.div<{ isActive: boolean }>`
+  border-top: 2px solid white;
+  border-left: 2px solid white;
+  transition: all 0.1s ease;
+
+  &:active {
+    border-top: none;
+    border-left: none;
+    border-bottom: 2px solid rgb(236, 239, 240);
+    border-right: 2px solid rgb(236, 239, 240);
+  }
+
+  ${({ isActive }) =>
+    isActive &&
+    css`
+      border-top: none;
+      border-left: none;
+      border-bottom: 2px solid rgb(236, 239, 240);
+      border-right: 2px solid rgb(236, 239, 240);
+    `}
+`;
+
+export const ButtonContainer = styled.div<{
+  isActive: boolean;
+  css?: ReturnType<typeof cssType>;
+}>`
+  background-color: rgb(236, 239, 240);
+  color: black;
+  transition: all 0.1s ease;
+  ${({ css }) => css}
+
+  &:active {
+    background-color: rgb(1, 0, 157);
+    color: white;
+  }
+
+  ${({ isActive }) =>
+    isActive &&
+    css`
+      background-color: rgb(1, 0, 157);
+      color: white;
+    `}
+`;
