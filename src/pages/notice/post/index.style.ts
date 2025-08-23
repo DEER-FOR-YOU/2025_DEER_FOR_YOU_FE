4b4df85bcc6437d426d004756880d9cc5005a4ba
chahyunwoo00 <ckgusdn8@naver.com>
2025-08-24 00:13:53 +0900

    feat: 공지사항 제목 input

--- src/pages/notice/post/index.style.ts
+++ src/pages/notice/post/index.style.ts
@@ -1,3 +1,35 @@
 import styled from '@emotion/styled';
 
-export const Container = styled.div``;
+export const Container = styled.div`
+  position: relative;
+`;
+
+export const Button = styled.img`
+  position: absolute;
+  top: 10px;
+  right: 10px;
+  cursor: pointer;
+`;
+
+export const TitleContainer = styled.div`
+  padding: 0 20px;
+  margin-top: 40px;
+  margin-bottom: 24px;
+`;
+
+export const Title = styled.input`
+  width: 100%;
+  padding: 12px 16px;
+  background-color: #f1f3f5;
+
+  color: #7f8992;
+  font-family: Pretendard;
+  font-size: 1.6rem;
+  font-style: normal;
+  font-weight: 500;
+  line-height: 100%; /* 16px */
+  letter-spacing: -0.4px;
+
+  border: none;
+  outline: none;
+`;
--- src/pages/notice/post/index.tsx
+++ src/pages/notice/post/index.tsx
@@ -1,6 +1,16 @@
 import React from 'react';
 import * as S from './index.style';
+import Header from '../../../components/header';
+import send from '../../../assets/send.svg';
 
 export default function NoticePostPage() {
-  return <S.Container>NoticePostPage</S.Container>;
+  return (
+    <S.Container>
+      <Header title="공지사항" />
+      <S.Button src={send} alt="send" />
+      <S.TitleContainer>
+        <S.Title placeholder="제목을 입력하세요" />
+      </S.TitleContainer>
+    </S.Container>
+  );
 }
