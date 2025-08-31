7f1927d541467bd9582c53675cf5d62f3d1e2829
chahyunwoo00 <ckgusdn8@naver.com>
2025-08-31 15:43:55 +0900

    chore: 느낌표 제거

--- src/pages/mypage/components/GuestBlur.tsx
+++ src/pages/mypage/components/GuestBlur.tsx
@@ -11,7 +11,7 @@ const GuestBlur: React.FC<Props> = ({ onLoginClick, OVERLAY_ID, blurRef }) => {
   return (
     <S.BlurContainer id={OVERLAY_ID} data-deerforyou-guard="1" ref={blurRef}>
       <S.BlurText>지금 로그인하고</S.BlurText>
-      <S.BlurText>관심 콘텐츠를 한 곳에서 이용해보세요!</S.BlurText>
+      <S.BlurText>관심 콘텐츠를 한 곳에서 이용해보세요</S.BlurText>
       <S.BlurButton onClick={onLoginClick}>로그인/회원가입</S.BlurButton>
     </S.BlurContainer>
   );
